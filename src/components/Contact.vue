84617e415d4cba14819a8171a0a40b5b0ba2ca99
Kinara <kinara_fakhrunnisa_27rpl@student.smktelkom-mlg.sch.id>
2020-09-01 20:32:54 +0700

    success

---
+++ src/components/Contact.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="container">
+        <h1>Contact page</h1>
+        This is Contact page
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Contact"
+}
+</script>
